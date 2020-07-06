53522aae56ba49a7451409525949b7bfdec62844
Csharp062020 <67912065+Csharp062020@users.noreply.github.com>
2020-07-07 00:32:46 +0300

    Add files via upload

---
+++ Computer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace _14062020
+{
+    //[DebuggerDisplay("Model {_model}")]
+    [DebuggerDisplay ("Price in INS {_price*3.67}")]
+    class Computer
+    {
+        public string _model;
+        public int _price;
+        public int _numberOfProcessors;
+        public float _screenSize;
+        public bool _isTurnOn;
+
+        public void tellMeThePrice()
+        {
+            Console.WriteLine($"price: {_price}");
+        }
+
+        public string tellMeTheScreenSize()
+        {
+            return _screenSize.ToString();
+        }
+
+
+        public void turnOn()
+        {
+            _isTurnOn = true;
+        }
+
+        public void turnOff()
+        {
+            _isTurnOn = false;
+        }
+
+        public void addProcessor()
+        {
+            _numberOfProcessors+= Convert.ToInt32(Console.ReadLine());
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()} {_model} {_price} {_numberOfProcessors} {_screenSize} {_isTurnOn}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14062020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer aComputer = new Computer()
+            {
+                _numberOfProcessors = 0,
+                _isTurnOn = false,
+                _model = "i7",
+                _price = 1000,
+                _screenSize = 16
+            };
+
+            Computer bComputer = new Computer()
+            {
+                _numberOfProcessors = 0,
+                _isTurnOn = false,
+                _model = "i5",
+                _price = 2000,
+                _screenSize = 17
+            };
+
+            Computer cComputer = new Computer()
+            {
+                _numberOfProcessors = 0,
+                _isTurnOn = false,
+                _model = "i9",
+                _price = 3000,
+                _screenSize = 18
+            };
+
+            Console.WriteLine($"you have {aComputer._numberOfProcessors}  how many processor to add");
+            aComputer.addProcessor();
+            Console.WriteLine($"Now you have {aComputer._numberOfProcessors} ");
+
+            Console.WriteLine($"Pc status:  {bComputer._isTurnOn} ");
+            bComputer.turnOn();
+            Console.WriteLine($"Pc status:  {bComputer._isTurnOn} ");
+
+            Console.WriteLine($"screen size:  {cComputer.tellMeTheScreenSize()} ");
+
+
+            Console.WriteLine(aComputer.ToString());
+            Console.WriteLine(bComputer.ToString());
+            Console.WriteLine(cComputer.ToString());
+
+
+
+        }
+    }
+
+}
